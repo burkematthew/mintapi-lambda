b2874bdc3279e1767d8995f9d7b60c63e4f57c9a
Matthew Burke <burkematthewjoseph@gmail.com>
2022-04-23 13:11:51 -0500

    [FEATURE] - Test V1

---
+++ function/execute.py
@@ -0,0 +1,24 @@
+import atexit
+import keyring
+import mintapi
+
+EMAIL = "burkematthewjoseph@gmail.com"
+
+
+def lambda_handler(event, context):
+    password = keyring.get_password("mintapi", EMAIL)
+    imap_password = keyring.get_password("mintapi_imap", EMAIL)
+
+    mint = mintapi.Mint(
+        EMAIL,
+        password,
+        mfa_method="email",
+        imap_server="imap.gmail.com",
+        imap_account=EMAIL,
+        imap_password=imap_password,
+        headless=True,
+    )
+    atexit.register(mint.close)  # Ensure everything is torn down.
+
+    investments = mint.get_investment_data()
+    return investments
